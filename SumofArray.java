389661527fae7720532ff25e097b4a1eff32a501
Maks <maksymilian.spisjak@gmail.com>
2022-07-27 23:45:29 +0200

    Added comments

--- SumofArray.java
+++ SumofArray.java
@@ -1,11 +1,11 @@
 public class SumofArray {
     public static void main(String[] args) {
-        int[] numArr={1,2,3,4,5,6};
+        int[] numArr={1,3,20,4,3,2};        //initializing an array with random numbers
         for (int i: sumOfArray(numArr))
             System.out.println(i);
     }
-    public static int[] sumOfArray(int[] intArr){
-        int[] sumArr=new int[intArr.length];
+    public static int[] sumOfArray(int[] intArr){   //the quickest solution saving the result in a new array,
+        int[] sumArr=new int[intArr.length];        //not changing the initial variables
         sumArr[0]=intArr[0];
         for(int i=1;i<sumArr.length;i++)
             sumArr[i]=intArr[i]+sumArr[i-1];
